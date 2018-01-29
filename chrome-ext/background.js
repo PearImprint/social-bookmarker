4076cb0003ac5be5bdc71406947ee64d37a2ea1d
jlee29 <jlee29@stanford.edu>
2018-01-28 16:32:00 -0800

    still trying to add user id

--- chrome-ext/background.js
+++ chrome-ext/background.js
@@ -1,1 +1,61 @@
-document.getElementById(siteIDs[i]).innerHTML = ""; //Yahoo
+// document.getElementById(siteIDs[i]).innerHTML = ""; //Yahoo
+console.log("running bakground");
+function xhrWithAuth(callback) {
+  var access_token;
+  var retry = true;
+  var url = 'https://www.googleapis.com/plus/v1/people/me';
+  getToken();
+  console.log("aishfs");
+  function getToken() {
+      chrome.identity.getAuthToken({ interactive: false }, function(token) {
+        if (chrome.runtime.lastError) {
+          callback(chrome.runtime.lastError);
+          console.log("Error", chrome.runtime.lastError);
+          return;
+        }
+        access_token = token;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', url);
+        xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+        xhr.onload = requestComplete;
+        xhr.send();
+      });
+    }
+
+    function requestComplete() {
+      if (this.status == 401 && retry) {
+        retry = false;
+        chrome.identity.removeCachedAuthToken({ token: access_token },
+                                              getToken);
+      } else {
+        callback(null, this.status, this.response);
+      }
+    }
+}
+
+function onUserInfoFetched(error, status, response) {
+  if (!error && status == 200) {
+    var user_info = JSON.parse(response);
+    if (user_info.emails) {
+      for (i = 0; i < user_info.emails.length; i++) {
+        emails.push(user_info.emails[i].value);
+      }
+      console.log("Found emails:", emails);
+    }
+  } else {
+    console.log("Error:", error);
+  }
+}
+
+/*** Return the email addresses to content.js ***/
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log("Sending emails:", emails);
+  sendResponse( {emails: emails})
+});
+
+
+/*** Main ***/
+console.log("Running one:none v1.3")
+var emails = [];
+xhrWithAuth(onUserInfoFetched);
--- chrome-ext/contentscript.js
+++ chrome-ext/contentscript.js
@@ -1,16 +1,24 @@
 // Separation of concerns: contentscript.js is just an interface to the html on the webpage. no logic here.
-
+console.log("running content script");
 // debugging: this code prints in the regular webpage console.
+chrome.extension.sendMessage({}, function(response) {
+  console.log(response);
+  if (response.emails.length > 0) {
+    console.log("response.emails is", response.emails);
+    profile_user = response.emails[0].split('@')[0];
+    console.log("Got profile user:", profile_user);
+  } else {
+    console.log("Couldn't get email address of chrome user.");
+  }
+});
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		console.log("received a request");
 		console.log(request);
-		document.getElementById("rhs").innerHTML = "";
 		if (request.message == "getTitle") {
-			console.log("received a request for page title, responding.");
+			console.log("received a request for page title, responding1.");
 			sendResponse({message: document.title});
-			chrome.identity.getProfile
 		}
 	}
 );
