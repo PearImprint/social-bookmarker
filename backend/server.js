f763aef6cb84e3765d616b89e5e7dc044cc52e1b
Peter Lu <6peterlu@gmail.com>
2018-02-02 20:59:05 -0800

    quotation marks changes

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 // debugging: this code prints in the local host terminal window.
 "use strict";
 
-var mongoose = require('mongoose');
-var express = require('express');
-var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
-var models = require('./schemas/models');
-var user = require('./user')
+var models = require("./schemas/models");
+var user = require("./user")
 
 const DATABASE_NAME = "data";
 const MONGO_URL = "mongodb://localhost:27017/" + DATABASE_NAME;
@@ -19,13 +19,13 @@ mongoose.connect(process.env.MONGODB_URI || MONGO_URL); // questionable.
 
 var server = app.listen(process.env.PORT || 3000, function() {
 	var port = server.address().port;
-	console.log('Listening at http://localhost:' + port + ' exporting the directory ' + __dirname);
+	console.log("Listening at http://localhost:" + port + " exporting the directory " + __dirname);
 });
 
 // Any data that is saved? 
 // TODO: reorganize server side code around which schemas are being called.
-app.post('/save', function(req, res) {
-	console.log('received POST request to /save');
+app.post("/save", function(req, res) {
+	console.log("received POST request to /save");
 	console.log(req.body);
 	if (req.body.type == "imprint") {
 		console.log("saving imprint entry");
