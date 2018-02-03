c0dec40c736b75375c9cb0e924957d8e574fabca
jlee29 <jlee29@stanford.edu>
2018-02-02 20:58:24 -0800

    write new commuNities to server

--- chrome-ext/popup.js
+++ chrome-ext/popup.js
@@ -4,18 +4,18 @@
 const IS_LOCAL = false;
 
 var USER_ID;
-const ROOT_URL = 'http://localhost:3000/';
+const ROOT_URL = "http://localhost:3000/";
 
-function post(path, params, method) { // should be able to use FormData for this but doesn't work :(
+function post(path, params, method) { // should be able to use FormData for this but doesn"t work :(
 
-    method = method || 'POST'; // Set method to post by default if not specified.
+    method = method || "POST"; // Set method to post by default if not specified.
 
     var request = new XMLHttpRequest();
     if (IS_LOCAL) {
-        request.open(method, 'http://localhost:3000/save', true);
+        request.open(method, "http://localhost:3000/save", true);
     } else {
         console.log("making heroku request");
-        request.open(method, 'http://pear-imprint.herokuapp.com/save', true);
+        request.open(method, "http://pear-imprint.herokuapp.com/save", true);
     }
 
     request.setRequestHeader("Content-type", "application/json");
@@ -27,38 +27,41 @@ function post(path, params, method) { // should be able to use FormData for this
 
     request.onreadystatechange = function() { //Call a function when the state changes.
         if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
-            console.log('tings finished');
+            console.log("tings finished");
         }
     }
 };
 
 function vote(voteValue) { // TODO: request other data from page.
-	console.log('vote button clicked');
+	console.log("vote button clicked");
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.extension.sendMessage({}, function(response) {
             USER_ID = response.id;
-            chrome.tabs.sendMessage(tabs[0].id, {message: 'getTitle'}, function(response) {
-                console.log('title found was: ' + response.title);
-                console.log('id for user is: ' + USER_ID);
+            chrome.tabs.sendMessage(tabs[0].id, {message: "getTitle"}, function(response) {
+                console.log("title found was: " + response.title);
+                console.log("id for user is: " + USER_ID);
                 console.log(response.url)
-                const data = {'title': response.title, 'url': response.url, 'user_id': USER_ID, 'vote': voteValue, 'type': 'imprint'};
-                post('save', data);
+                const data = {"title": response.title, "url": response.url, "user_id": USER_ID, "vote": voteValue, "type": "imprint"};
+                post("save", data);
             });
         })
 	});
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('upvote').addEventListener('click', function() { vote(1) });
-    document.getElementById('downvote').addEventListener('click', function() { vote(-1) });
-    document.getElementById('add').addEventListener("click", changeCommunity);
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("upvote").addEventListener("click", function() { vote(1) });
+    document.getElementById("downvote").addEventListener("click", function() { vote(-1) });
+    document.getElementById("add").addEventListener("click", addNewCommunity);
 });
 
-function changeCommunity() {
-    var dropdown = document.getElementById('dropdown');
-    var newCommunity = document.getElementById('newCommunity').value;
+function addNewCommunity() {
+    var dropdown = document.getElementById("dropdown");
+    var newCommunity = document.getElementById("newCommunity").value;
     dropdown.options[dropdown.options.length] = new Option(newCommunity, newCommunity);
     dropdown.selectedIndex = dropdown.options.length-1;
+
+    const data = {"name": newCommunity, "google_id": USER_ID, type: "community"}
+    post("save", data)
 }
 
 
